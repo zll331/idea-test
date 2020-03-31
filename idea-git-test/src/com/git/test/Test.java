4bdab6944afcfbdd98864f47e9ae6fbd2d43fbc8
zll <201175954@qq.com>
2020-04-01 00:16:25 +0800

    把git-test添加到git

---
+++ idea-git-test/src/com/git/test/Test.java
@@ -0,0 +1,10 @@
+package com.git.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
